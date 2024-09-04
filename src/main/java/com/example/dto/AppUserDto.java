00a48616ac69063b4ee51b2f95e2eb3c07d9aa91
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-09-05 00:31:17 +0530

    added EntityManager use case

---
+++ src/main/java/com/example/dto/AppUserDto.java
@@ -0,0 +1,4 @@
+package com.example.dto;
+
+public record AppUserDto(long userId, String email, String password, String firstName, String lastName) {
+}
--- src/main/java/com/example/repository/CustomUserRepoImpl.java
+++ src/main/java/com/example/repository/CustomUserRepoImpl.java
@@ -1,12 +1,10 @@
 package com.example.repository;
 
+import com.example.dto.AppUserDto;
 import com.example.entity.AppUser;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 
 import java.util.List;
 
@@ -15,15 +13,21 @@ public class CustomUserRepoImpl implements CustomUserRepo {
     @PersistenceContext
     private EntityManager entityManager;
 
+    private static AppUser convert(AppUserDto appUserDto) {
+        return new AppUser(appUserDto.userId(), appUserDto.email(), appUserDto.password(), appUserDto.firstName(), appUserDto.lastName(), null);
+    }
+
     @Override
     public List<AppUser> getAllUsersByEmail(String email) {
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<AppUser> cr = cb.createQuery(AppUser.class);
-        Root<AppUser> root = cr.from(AppUser.class);
-        cr.select(root).where(cb.like(root.get("email"), "%" + email + "%"));
-
-        Query query = entityManager.createQuery(cr);
-        List<AppUser> results = query.getResultList();
-        return results;
+        long startTime = System.currentTimeMillis();
+        // Execute the query
+        Query query = entityManager.createQuery("SELECT new com.example.dto.AppUserDto(u.userId, u.email, u.password, u.firstName, u.lastName) FROM AppUser u where u.email like :email");
+        query.setParameter("email", "%" + email + "%");
+        List<AppUserDto> users = query.getResultList();
+        entityManager.close();
+        long endTime = System.currentTimeMillis();
+        System.out.println(users);
+        System.out.println("Time taken : " + (endTime - startTime));
+        return users.stream().map(CustomUserRepoImpl::convert).toList();
     }
 }
